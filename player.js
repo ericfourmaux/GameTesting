c666b5c3333a1ff3dd8014968918ec3eeb904efe
EricF <ericfourmaux@users.noreply.github.com>
2018-12-17 08:39:02 -0500

    Firing a laser

---
+++ player.js
@@ -0,0 +1,17 @@
+var player = {
+  x : 200,
+  y : 225,
+  h : 15,
+  w : 10,
+
+  vx : 0,
+  vy : 0,
+
+  isOnGround : false,
+
+  friction : 0.96,
+  gravity : 0.3,
+
+  speedLimit : -10,
+  jumpForce : .1
+}
